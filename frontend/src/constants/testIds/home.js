fa01fd2044de6851ae6bbf4f3c1df5155cd2be61
Manav Agarwal <manavagarwal193@gmail.com>
2026-09-16 13:08:55 +0530

    cleanup: remove obsolete provider test id

--- frontend/src/constants/testIds/home.js
+++ frontend/src/constants/testIds/home.js
@@ -1,6 +1,4 @@
-// Test IDs for the home / landing feature. Naming follows the directive
-// in ./auth.js (keys camelCase, values kebab-case `<feature>-<element>`).
+// Test IDs for the home / landing feature.
+// Keys are camelCase; values are kebab-case `<feature>-<element>`.
 
-export const HOME = {
-	emergentLink: 'home-emergent-link',
-};
+export const HOME = {};
